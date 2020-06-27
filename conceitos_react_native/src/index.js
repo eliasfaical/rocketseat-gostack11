a31af03cd67dcc99e13e7a6f6b429bf32036a04e
Elias Faiçal <eliasfaical@gmail.com>
2020-06-27 19:53:29 -0300

    Conceitos React Native

--- conceitos_react_native/src/index.js
+++ conceitos_react_native/src/index.js
@@ -1,28 +1,77 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+
+function App() {
+  const [ projects, setProjects ] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    })
+  }, []);
+
+  async function handleAddProjects() {
+    const response = await api.post('projects', {
+      title: `Novo Job ${Date.now()}`,
+      owner: 'Elias Faical'
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+      <SafeAreaView style={styles.container}>
+        <FlatList 
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({item: project}) => (
+          <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+
+        <TouchableOpacity 
+          activeOpacity={0.6} 
+          style={styles.button} 
+          onPress={handleAddProjects}
+        >
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 
-  title: {
+  project: {
     color: '#FFF',
+    fontSize: 20,
+  },
+
+  button: {
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
   }
 });
-
-function App() {
-  return (
-    <>
-      <StatusBar  barStyle="light-content" backgroundColor="#7159c1" />
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello Rocketseat</Text>
-      </View>
-    </>
-  );
-}
 
 export default App;
